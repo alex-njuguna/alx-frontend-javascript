e16b36c4eb17e39b6ea85160812cfe68aef766f5
alex-njuguna <njugunakinuthia013@gmail.com>
2023-11-27 22:15:42 +0300

    correction on for loops task

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,4 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const newArray = array.map((value) => appendString + value);
+  const newArray = array;
+  for (const value of array) {
+    const idx = array.indexOf(value);
+    newArray[idx] = appendString + value;
+  }
+
   return newArray;
 }
