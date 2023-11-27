18197798a0891e8886ad58334e4171ff2650fde0
alex-njuguna <njugunakinuthia013@gmail.com>
2023-11-27 19:36:24 +0300

    eslint styling

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -5,6 +5,9 @@ export default function taskBlock(trueOrFalse) {
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
+    if (task && task2) {
+      return [task, task2];
+    }
   }
 
   return [task, task2];
