bc5b62590e8550fc2ea2fa0af402579943610b38
alex-njuguna <njugunakinuthia013@gmail.com>
2023-11-28 21:45:45 +0300

    returning a promise

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+function getResponseFromAPI() {
+  const success = 'some message here';
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(Error('API not working'));
+    }
+  });
+}
