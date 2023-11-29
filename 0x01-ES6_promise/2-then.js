e34aee63b0255f3bc7c8ccff67b120830bdf9ae7
alex-njuguna <njugunakinuthia013@gmail.com>
2023-11-29 12:45:30 +0300

    creating a simple promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,9 @@
 function handleResponseFromAPI(promise) {
-  promise
-    .then(() => {
-      console.log('Got a response from the API');
-      return {
-        status: 200,
-        body: 'Success',
-      };
-    })
+  const body = { status: 200, body: 'success' };
+
+  return promise
+    .then(() => body)
     .catch((error) => error)
     .finally(() => console.log('Got a response from the API'));
 }
-
 export default handleResponseFromAPI;
