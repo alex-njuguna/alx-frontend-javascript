1db15f95f1806012a65bfa26fbb9e252aeef98b7
alex-njuguna <njugunakinuthia013@gmail.com>
2023-11-30 19:44:54 +0300

    creates a SkyHighBuilding class that inherits from Building class

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -2,6 +2,9 @@ import Building from './5-building';
 
 class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
+    if (typeof floors !== 'number' || floors < 1) {
+      throw new Error('Inavlid input');
+    }
     super(sqft);
     this._floors = floors;
   }
@@ -11,7 +14,11 @@ class SkyHighBuilding extends Building {
   }
 
   set floors(value) {
-    this._floors = value;
+    if (typeof value !== 'number' || value < 1) {
+      throw new Error('Floors must be a number');
+    } else {
+      this._floors = value;
+    }
   }
 
   evacuationWarningMessage() {
