b5ad77c3393b271e007b636845a307f2f0f18a1b
alex-njuguna <njugunakinuthia013@gmail.com>
2023-11-30 20:37:28 +0300

    hoisting

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -42,6 +42,5 @@ const student5 = new StudentHolberton('Jason', 'Sandler', new HolbertonClass(201
 
 const listOfStudents = [student1, student2, student3, student4, student5];
 console.log(listOfStudents);
-console.log(listOfStudents.map(student => student.fullStudentDescription));
-export {listOfStudents};
-
+console.log(listOfStudents.map((student) => student.fullStudentDescription));
+export { listOfStudents };
