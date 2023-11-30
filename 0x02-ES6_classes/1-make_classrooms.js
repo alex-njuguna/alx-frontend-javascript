1f1ee904c319892a2b5549ed4404796b77e877ef
alex-njuguna <njugunakinuthia013@gmail.com>
2023-11-30 15:30:00 +0300

    makes more classrooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,12 +1,13 @@
-import ClassRoom from "./0-classroom";
+import ClassRoom from './0-classroom';
 
-function initializeRooms(){
-    const class1 = new ClassRoom(19)
-    const class2 = new ClassRoom(20)
-    const class3 = new ClassRoom(34)
+function initializeRooms() {
+  const class1 = new ClassRoom(19);
+  const class2 = new ClassRoom(20);
+  const class3 = new ClassRoom(34);
 
-    const classes = [class1, class2, class3]
+  const classes = [class1, class2, class3];
 
-    return classes
-
+  return classes;
 }
+
+export default initializeRooms;
