bd9e7328b708dcef741ed461086142c82bd7aa3c
alex-njuguna <njugunakinuthia013@gmail.com>
2023-11-30 17:07:27 +0300

    adds pricing class and associated methods

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,51 @@
+import Currency from './3-currency';
+
+class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number' || !(currency instanceof Currency)) {
+      throw new Error('Invalid input');
+    }
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new Error('Invalid input');
+    } else {
+      this._amount = value;
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new Error('Invalid input');
+    } else {
+      this._currency = value;
+    }
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new Error('Invald input type');
+    } else {
+      const convertedAmount = amount * conversionRate;
+
+      return convertedAmount;
+    }
+  }
+}
+
+export default Pricing;
