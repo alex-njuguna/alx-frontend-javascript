940f3176fc01232318dc039161b62f5005c9b238
alex-njuguna <njugunakinuthia013@gmail.com>
2023-11-30 20:06:02 +0300

    creates Airport class

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,12 @@
+class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get [Symbol.toStringTag]() {
+    return `${this._code}`;
+  }
+}
+
+export default Airport;
