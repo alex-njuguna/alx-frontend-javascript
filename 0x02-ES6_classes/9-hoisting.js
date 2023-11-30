481a29badd53723ec6c871d3ab3c7c53136b20b9
alex-njuguna <njugunakinuthia013@gmail.com>
2023-11-30 21:24:53 +0300

    hoisting

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -33,14 +33,13 @@ export class StudentHolberton {
   }
 }
 
+const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
+
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
-const student3 = new StudentHolberton('Albert', 'Clinton', new HolbertonClass(2019, 'San Francisco'));
-const student4 = new StudentHolberton('Donald', 'Bush', new HolbertonClass(2019, 'San Francisco'));
-const student5 = new StudentHolberton('Jason', 'Sandler', new HolbertonClass(2019, 'San Francisco'));
+const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-const listOfStudents = [student1, student2, student3, student4, student5];
-console.log(listOfStudents);
-console.log(listOfStudents.map((student) => student.fullStudentDescription));
-export { listOfStudents };
+export const listOfStudents = [student1, student2, student3, student4, student5];
