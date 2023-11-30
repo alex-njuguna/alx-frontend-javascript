57efe4fe71ff8fceedb6f3b51e0015d2c6c4678a
alex-njuguna <njugunakinuthia013@gmail.com>
2023-11-30 20:22:00 +0300

    primitive symbols

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,15 @@
+class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
+    }
+    return this._location;
+  }
+}
+
+export default HolbertonClass;
