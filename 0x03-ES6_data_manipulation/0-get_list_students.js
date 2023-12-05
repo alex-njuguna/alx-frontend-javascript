8d2b7a16a0571ac5af5fb78a5396b8ef455942cf
alex-njuguna <njugunakinuthia013@gmail.com>
2023-12-05 19:05:42 +0300

    basic list of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,23 @@
+function getListStudents() {
+  const students = [
+    {
+      firstName: 'Guillaume',
+      id: 1,
+      location: 'San Francisco',
+    },
+    {
+      firstName: 'James',
+      id: 2,
+      location: 'Columbia',
+    },
+    {
+      firstName: 'Serena',
+      id: 5,
+      location: 'San Francisco',
+    },
+  ];
+
+  return students;
+}
+
+export default getListStudents;
